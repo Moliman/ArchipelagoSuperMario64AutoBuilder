f0fc63b9c574cc71d8e5f832a05208575083685f
Moliman <ostp22@outlook.com>
2023-11-01 15:17:04 -0400

    Add pause on failure
    Signed-off-by: Moliman <ostp22@outlook.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 read -p "Path of Super Mario 64 US: " -r sm64rom
 
+set -x
+
 pacman -Syuu
 
 pacman -S unzip make git mingw-w64-i686-gcc mingw-w64-x86_64-gcc mingw-w64-i686-glew mingw-w64-x86_64-glew mingw-w64-i686-SDL2 mingw-w64-i686-SDL mingw-w64-x86_64-SDL2 mingw-w64-x86_64-SDL python3
@@ -18,3 +20,8 @@ git apply enhancements/nonstop_mode_always_enabled.patch
 cp ${sm64romunixpath} baserom.us.z64
 
 make EXTERNAL_DATA=0 VERSION=us -j2
+
+if [[ $? -ne 0 ]]; then
+    read -p "The build has failed"
+    exit -1
+fi
